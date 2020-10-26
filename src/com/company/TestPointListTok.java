65523e062b3006f2dfcbdf3fccc37e62dbd599dd
Michael Shevelin <shevel55505@gmail.com>
2020-10-27 00:40:52 +0200

    working on tokenizer

--- src/com/company/TestPointListTok.java
+++ src/com/company/TestPointListTok.java
@@ -1,9 +1,49 @@
 package com.company;
+import javax.xml.transform.Templates;
 import java.io.*;
 import java.awt.Point;
 
 public class TestPointListTok {
     InputStreamReader reader; // = new InputStreamReader(System.in);
     StreamTokenizer tokens; // = new StreamTokenizer(reader);
+    ArrayPointList list;
 
+    TestPointListTok(ArrayPointList arr){
+        list = arr;
+        reader = new InputStreamReader(System.in);
+        tokens = new StreamTokenizer(reader);
+    }
+
+    void run() throws Exception {
+        Point p = new Point();
+        label:
+        while (true) {
+            if((tokens.nextToken()) == StreamTokenizer.TT_WORD){
+                switch (tokens.sval) {
+                    case "quit":
+                        break label;
+                    case "add":
+                        tokens.nextToken();
+                        p.x = (int) tokens.nval;
+                        tokens.nextToken();
+                        p.y = (int) tokens.nval;
+                        list.append(p);
+                        break;
+                    case "curr":
+                        System.out.println(list.getCursor());
+                        break;
+                    case "next":
+                        System.out.println(list.goToNext());
+                        break;
+                    case "start":
+                        System.out.println(list.goToBeginning());
+                        break;
+                }
+            }   else if(tokens.sval.equals("clear")){
+                list.clear();
+
+            }
+        }
+    }
 }
+
